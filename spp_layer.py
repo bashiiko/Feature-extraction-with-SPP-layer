9ef13c260b65050c32e02df060d53eafdafd076d
bashiiko <k1930047@edu.cc.uec.ac.jp>
2019-11-07 20:36:26 +0900

    [fix] spp_layer.pyのstrideの計算を修正

--- spp_layer.py
+++ spp_layer.py
@@ -1,6 +1,13 @@
 # From https://github.com/yueruchen/sppnet-pytorch
 
 import math
+
+##############################################################################
+# 追加
+import torch
+import torch.nn as nn
+##############################################################################
+
 def spatial_pyramid_pool(self,previous_conv, num_sample, previous_conv_size, out_pool_size):
     '''
     previous_conv: a tensor vector of previous convolution layer
@@ -12,12 +19,25 @@ def spatial_pyramid_pool(self,previous_conv, num_sample, previous_conv_size, out
     '''
     # print(previous_conv.size())
     for i in range(len(out_pool_size)):
-        # print(previous_conv_size)
+        #print(previous_conv_size)
         h_wid = int(math.ceil(previous_conv_size[0] / out_pool_size[i]))
         w_wid = int(math.ceil(previous_conv_size[1] / out_pool_size[i]))
+
+        ##############################################################################
+        # 追加
+        h_str = int(math.floor(previous_conv_size[0] / out_pool_size[i]))
+        w_str = int(math.floor(previous_conv_size[1] / out_pool_size[i]))
+        ##############################################################################
+
         h_pad = (h_wid*out_pool_size[i] - previous_conv_size[0] + 1)/2
         w_pad = (w_wid*out_pool_size[i] - previous_conv_size[1] + 1)/2
-        maxpool = nn.MaxPool2d((h_wid, w_wid), stride=(h_wid, w_wid), padding=(h_pad, w_pad))
+
+        ##############################################################################
+        # 変更
+        maxpool = nn.MaxPool2d((h_wid, w_wid), stride=(h_str, w_str), padding=(0,0))
+        #maxpool = nn.MaxPool2d((h_wid, w_wid), stride=(h_wid, w_wid), padding=(h_pad, w_pad))
+        ##############################################################################
+
         x = maxpool(previous_conv)
         if(i == 0):
             spp = x.view(num_sample,-1)
